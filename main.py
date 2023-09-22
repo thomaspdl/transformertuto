af224c76de4db452135f2ec69a46cdc9634b8232
Thomas Pumir <pumir.thomas@gmail.com>
2023-09-22 16:14:22 +0200

    more code

--- main.py
+++ main.py
@@ -21,3 +21,25 @@ val_data = data_process(val_iter)
 test_data = data_process(test_iter)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def batchify(data: Tensor, bsz: int) -> Tensor:
+    """Divides the data into ``bsz`` separate sequences, removing extra elements
+    that wouldn't cleanly fit.
+
+    Arguments:
+        data: Tensor, shape ``[N]``
+        bsz: int, batch size
+
+    Returns:
+        Tensor of shape ``[N // bsz, bsz]``
+    """
+    seq_len = data.size(0) // bsz
+    data = data[:seq_len * bsz]
+    data = data.view(bsz, seq_len).t().contiguous()
+    return data.to(device)
+
+batch_size = 20
+eval_batch_size = 10
+train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
+val_data = batchify(val_data, eval_batch_size)
+test_data = batchify(test_data, eval_batch_size)
