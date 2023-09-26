3291c632ba058ef34cb65097e04693c72c8d81ae
Thomas Pumir <pumir.thomas@gmail.com>
2023-09-26 11:39:05 +0200

    transformer tuto

--- main.py
+++ main.py
@@ -4,30 +4,20 @@ from torchtext.vocab import build_vocab_from_iterator
 import torch
 from transformer import TransformerModel
 
+import time
+import torch.nn as nn
+
+import os as os 
+
 
 # source: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 
-
-train_iter = WikiText2(split='train')
-tokenizer = get_tokenizer('basic_english')
-vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
-vocab.set_default_index(vocab['<unk>'])
-
-def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+def data_process(raw_text_iter):
     """Converts raw text into a flat Tensor."""
     data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
     return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
 
-# ``train_iter`` was "consumed" by the process of building the vocab,
-# so we have to create it again
-train_iter, val_iter, test_iter = WikiText2()
-train_data = data_process(train_iter)
-val_data = data_process(val_iter)
-test_data = data_process(test_iter)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-def batchify(data: Tensor, bsz: int) -> Tensor:
+def batchify(data, bsz: int):
     """Divides the data into ``bsz`` separate sequences, removing extra elements
     that wouldn't cleanly fit.
 
@@ -43,14 +33,7 @@ def batchify(data: Tensor, bsz: int) -> Tensor:
     data = data.view(bsz, seq_len).t().contiguous()
     return data.to(device)
 
-batch_size = 20
-eval_batch_size = 10
-train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
-val_data = batchify(val_data, eval_batch_size)
-test_data = batchify(test_data, eval_batch_size)
-
-bptt = 35
-def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
+def get_batch(source, i: int):
     """
     Args:
         source: Tensor, shape ``[full_seq_len, batch_size]``
@@ -64,23 +47,6 @@ def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
     data = source[i:i+seq_len]
     target = source[i+1:i+1+seq_len].reshape(-1)
     return data, target
-
-
-ntokens = len(vocab)  # size of vocabulary
-emsize = 200  # embedding dimension
-d_hid = 200  # dimension of the feedforward network model in ``nn.TransformerEncoder``
-nlayers = 2  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
-nhead = 2  # number of heads in ``nn.MultiheadAttention``
-dropout = 0.2  # dropout probability
-model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
-
-
-import time
-
-criterion = nn.CrossEntropyLoss()
-lr = 5.0  # learning rate
-optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
 
 def train(model: nn.Module) -> None:
     model.train()  # turn on train mode
@@ -124,10 +90,6 @@ def evaluate(model: nn.Module, eval_data: Tensor) -> float:
             total_loss += seq_len * criterion(output_flat, targets).item()
     return total_loss / (len(eval_data) - 1)
 
-
-best_val_loss = float('inf')
-epochs = 3
-
 with TemporaryDirectory() as tempdir:
     best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
 
@@ -148,6 +110,60 @@ with TemporaryDirectory() as tempdir:
 
         scheduler.step()
     model.load_state_dict(torch.load(best_model_params_path)) # load best model states
+
+
+
+
+train_iter = WikiText2(split='train')
+tokenizer = get_tokenizer('basic_english')
+vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
+vocab.set_default_index(vocab['<unk>'])
+
+
+# ``train_iter`` was "consumed" by the process of building the vocab,
+# so we have to create it again
+train_iter, val_iter, test_iter = WikiText2()
+train_data = data_process(train_iter)
+val_data = data_process(val_iter)
+test_data = data_process(test_iter)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+batch_size = 20
+eval_batch_size = 10
+train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
+val_data = batchify(val_data, eval_batch_size)
+test_data = batchify(test_data, eval_batch_size)
+
+bptt = 35
+
+
+
+ntokens = len(vocab)  # size of vocabulary
+emsize = 200  # embedding dimension
+d_hid = 200  # dimension of the feedforward network model in ``nn.TransformerEncoder``
+nlayers = 2  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
+nhead = 2  # number of heads in ``nn.MultiheadAttention``
+dropout = 0.2  # dropout probability
+model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
+
+
+
+criterion = nn.CrossEntropyLoss()
+lr = 5.0  # learning rate
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+import pdb; pdb.set_trace()
+
+
+
+
+
+best_val_loss = float('inf')
+epochs = 3
+
 
 
 test_loss = evaluate(model, test_data)
