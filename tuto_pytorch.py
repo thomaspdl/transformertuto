83b4492ed8f2b43c65768841da6944cd6acf55be
Thomas Pumir <pumir.thomas@gmail.com>
2023-09-26 16:40:01 +0200

    add file

---
+++ tuto_pytorch.py
@@ -0,0 +1,4 @@
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torchtext.datasets import multi30k, Multi30k
+from typing import Iterable, List
